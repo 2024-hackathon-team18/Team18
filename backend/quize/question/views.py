51b9693eeb227667495c220b076c3c94339a2c51
wndjs803 <wndjs803@naver.com>
2024-06-01 04:32:13 +0900

    feat: websocket 연결 중

--- backend/quize/question/views.py
+++ backend/quize/question/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 # Create your views here.
+
+class QuestonAPIView(APIView):
+    def post(self, request):
+        pass
+
+    def get(self, request):
+        pass
