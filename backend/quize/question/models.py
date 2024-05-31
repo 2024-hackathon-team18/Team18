81d59e2d43cf5a016595998ea1e315b89dc015a4
wndjs803 <wndjs803@naver.com>
2024-06-01 02:29:07 +0900

    chore: backend 초기 세팅

---
+++ backend/quize/question/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Question(models.Model):
+    text = models.TextField("문제 지문")
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
+    text = models.CharField("보기", max_length=255)
+    is_correct = models.BooleanField("정답 여부", default=False)
+
+class Explanation(models.Model):
+    question = models.OneToOneField(Question, related_name='explanation', on_delete=models.CASCADE)
+    text = models.TextField("해설")
+
