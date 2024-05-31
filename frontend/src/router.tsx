fc845a46cb9397db51c7eacc4350a96599372afb
mouhajii <qpwoeiruty1134@gmail.com>
2024-06-01 06:19:51 +0900

    test page publish

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import TestPage from './pages/TestPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <LandingPage />,
   },
+  {
+    path: '/test',
+    element: <TestPage />
+  }
 ]);
